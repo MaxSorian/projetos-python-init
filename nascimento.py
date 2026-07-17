fba7ce30edb8d4a5a1f286acd530dea51344a4d8
Maxmillian Dos Santos Soriano <maxmilliansoriano4@gmail.com>
2026-07-16 23:39:15 -0300

    Add files via upload

---
+++ nascimento.py
@@ -0,0 +1,6 @@
+input ('Vamos ver qual sua data de nascimento? ')
+dia = input ('Dia: ')
+mes = input ('Mês: ')
+ano = input ('Ano: ')
+input(f'Você nasceu no dia {dia} de {mes} de {ano} ., Correto? ')
+
